d4bde19764d3effcbe4af9a62798d3caa42f013a
m4cola <mike@mdinicola.com>
2021-03-14 04:51:38 -0400

    add contextmanager for opening and closing client

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import asyncio
 import aioharmony.exceptions
 
+from contextlib import asynccontextmanager
 from quart import Quart
 from quart import request
 from aioharmony.harmonyapi import HarmonyAPI, SendCommandDevice
@@ -25,15 +26,19 @@ async def close_client(client):
     if client:
         await asyncio.wait_for(client.close(), timeout=60)
 
+@asynccontextmanager
+async def open_client():
+    client = await get_client(_HUB_IP)
+    yield client
+    await close_client(client)
+
 @app.route("/health")
 async def get_health():
-    hub_client = await get_client(_HUB_IP)
-    await close_client(hub_client)
-
-    output = {
-        "harmonyHub": hub_client is not None
-    }
-    return output
+    async with open_client() as hub_client:
+        output = {
+            "healthy": hub_client is not None
+        }
+        return output
 
 if __name__ == "__main__":
     app.run()

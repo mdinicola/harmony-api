853e789e6ee43f96b7fc30bb8ad0e310b01b0837
m4cola <mike@mdinicola.com>
2021-03-14 19:23:05 -0400

    use an api key

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import confuse
 
+from functools import wraps
+from quart import Quart, request
 from helpers import open_client, get_device_from_config
 from helpers import validate_command_request, send_commands_to_device
-from quart import Quart, request
 
 app = Quart(__name__)
 config = confuse.Configuration('App', __name__)
 config.set_file('config/config.yml')
 
+API_KEY = config['api_key'].get()
 HUB_IP = config['hub_ip'].get()
 COMMAND_DELAY = config['command_delay'].get(int)
 
+def require_apikey(view_function):
+    @wraps(view_function)
+    async def decorated_function(*args, **kwargs):
+        if request.args.get('apikey') and request.args.get('apikey') == API_KEY:
+            return await view_function(*args, **kwargs)
+        else:
+            return { 'error': 'API Key not provided or not valid' }, 401
+    return decorated_function
+
 @app.route('/health')
+@require_apikey
 async def get_health():
     async with open_client(HUB_IP) as hub_client:
         output = {
@@ -20,6 +32,7 @@ async def get_health():
         return output
 
 @app.route('/sendCommandsToDevice', methods=['POST'])
+@require_apikey
 async def send_command():
     try:
         validate_command_request(request)
